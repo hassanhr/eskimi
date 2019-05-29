d29e9447457cf51b4a221a88882e3a72f281d5da
hassanhr <glucosemishu@gmail.com>
2019-05-29 15:50:42 +0600

    Add files via upload

---
+++ src/router.js
@@ -0,0 +1,47 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './views/Home.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/users',
+      name: 'users',
+      // route level code-splitting 
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/Users.vue')
+    },
+    {
+      path: '/datapage',
+      name: 'datapage',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/Datapage.vue')
+    },
+    {
+      path: '/highlight',
+      name: 'highlight',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/Highlight.vue')
+    },
+    {
+      path: '/highlight2',
+      name: 'highlight2',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/Highlight2.vue')
+    }
+  ]
+})
